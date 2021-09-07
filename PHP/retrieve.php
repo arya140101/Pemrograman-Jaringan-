f6517d64532bccb07d008ece212acbc52ccda505
Azarya Divino <90166684+arya140101@users.noreply.github.com>
2021-09-07 15:36:01 +0700

    Upload 6 file PHP
    Upload create.php, delete.php, get.php, koneksi.php, retrieve.php, update.php

---
+++ PHP/retrieve.php
@@ -0,0 +1,29 @@
+<?php 
+require("koneksi.php");
+$perintah = "SELECT * FROM tbl_laundry";
+$eksekusi = mysqli_query($konek, $perintah);
+$cek = mysqli_affected_rows($konek);
+
+if($cek > 0){
+	$response["kode"] = 1;
+	$response["pesan"] = "Data tersedia";
+	$response["data"] = array();
+
+	while($ambil = mysqli_fetch_object($eksekusi)){
+		$F["id"] = $ambil->id;
+		$F["nama"] = $ambil->nama;
+		$F["alamat"] = $ambil->alamat;
+		$F["telepon"] = $ambil->telepon;
+
+	array_push($response["data"], $F);
+
+	}
+
+}
+else{
+	$response["kode"] = 0;
+	$response["pesan"] = "Data tidak tersedia";
+}
+
+echo json_encode($response);
+mysqli_close($konek);
